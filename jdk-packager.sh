05d6b698bf876946373e1aa7089706aa448ff343
Stefan Ferstl <st.ferstl@gmail.com>
2017-01-29 22:51:27 +0100

    Download and extract file into target/ directory
    - Downloads will be located at target/downloads
    - Extracded files will be located in target/tmp

--- jdk-packager.sh
+++ jdk-packager.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Locations
-BASEDIR=`cd $(dirname $0); pwd`
+SCRIPTDIR=`cd $(dirname $0); pwd`
+SCRIPTNAME=`basename $0`
 WORKINGDIR=`pwd`
-SCRIPT_NAME=`basename $0`
 
 # configuration variables
 JAVA_VERSION_MAJOR=8
@@ -16,7 +16,7 @@ PROXY_SERVER=
 
 print_usage() {
    cat << EOF
-Usage: $SCRIPT_NAME <options>
+Usage: $SCRIPTNAME <options>
   <options>:
     -m    Java major version
     -u    Java update version
@@ -26,7 +26,7 @@ Usage: $SCRIPT_NAME <options>
     -h    Print this help message
 
   Example for server-jre 8u121:
-  $SCRIPT_NAME -m 8 -u 121 -b 13 -g e9e7ea248e2c4826b92b3f075a80e441
+  $SCRIPTNAME -m 8 -u 121 -b 13 -g e9e7ea248e2c4826b92b3f075a80e441
 
 EOF
 }
@@ -68,18 +68,23 @@ done
 
 
 # convenience variables
-ORIGINAL_PACKAGE=${JAVA_PACKAGE}-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_UPDATE}-linux-x64.tar.gz
-JCE_PACKAGE=jce_policy-${JAVA_VERSION_MAJOR}.zip
-JCE_DIRECTORY=UnlimitedJCEPolicyJDK${JAVA_VERSION_MAJOR}
-JDK_DIRECTORY=jdk1.${JAVA_VERSION_MAJOR}.0_${JAVA_VERSION_UPDATE}
-FINAL_ARTIFACT=target/${JAVA_PACKAGE}-1.${JAVA_VERSION_MAJOR}.0u${JAVA_VERSION_UPDATE}.tar.gz
+TARGET_DIR=${WORKINGDIR}/target
+DOWNLOAD_DIR=${TARGET_DIR}/download
+TEMP_DIR=${TARGET_DIR}/tmp
+ORIGINAL_PACKAGE=${DOWNLOAD_DIR}/${JAVA_PACKAGE}-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_UPDATE}-linux-x64.tar.gz
+JCE_PACKAGE=${DOWNLOAD_DIR}/jce_policy-${JAVA_VERSION_MAJOR}.zip
+JCE_DIRECTORY=${TEMP_DIR}/UnlimitedJCEPolicyJDK${JAVA_VERSION_MAJOR}
+JDK_DIRECTORY=${TEMP_DIR}/jdk1.${JAVA_VERSION_MAJOR}.0_${JAVA_VERSION_UPDATE}
+FINAL_ARTIFACT=${TARGET_DIR}/${JAVA_PACKAGE}-1.${JAVA_VERSION_MAJOR}.0u${JAVA_VERSION_UPDATE}.tar.gz
 
 # convenience functions
 download_from_oracle_com() {
+  url=$1
+  download_dir=$2
   if [ -z $PROXY_SERVER ]; then
-    curl -fLOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" $1
+    (cd "${download_dir}" && curl -fLOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" ${url})
   else
-    curl -fLOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" -x $PROXY_SERVER $1
+    (cd "${download_dir}" && curl -fLOH "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" -x $PROXY_SERVER ${url})
   fi
   if [ $? -ne 0 ]; then
     echo "Could not download artifact" 1>&2
@@ -87,21 +92,25 @@ download_from_oracle_com() {
   fi
 }
 
+# Create directories
+mkdir -p "${TARGET_DIR}"
+mkdir -p "${DOWNLOAD_DIR}"
+mkdir -p "${TEMP_DIR}"
+
 if [ ! -f $ORIGINAL_PACKAGE ]; then
-  download_from_oracle_com "http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION_MAJOR}u${JAVA_VERSION_UPDATE}-b${JAVA_VERSION_BUILD}${JAVA_VERSION_UUID}/${JAVA_PACKAGE}-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_UPDATE}-linux-x64.tar.gz"
+  download_from_oracle_com "http://download.oracle.com/otn-pub/java/jdk/${JAVA_VERSION_MAJOR}u${JAVA_VERSION_UPDATE}-b${JAVA_VERSION_BUILD}${JAVA_VERSION_UUID}/${JAVA_PACKAGE}-${JAVA_VERSION_MAJOR}u${JAVA_VERSION_UPDATE}-linux-x64.tar.gz" "${DOWNLOAD_DIR}"
 fi
 
 if [ ! -f $JCE_PACKAGE ]; then
-  download_from_oracle_com "http://download.oracle.com/otn-pub/java/jce/${JAVA_VERSION_MAJOR}/jce_policy-${JAVA_VERSION_MAJOR}.zip"
+  download_from_oracle_com "http://download.oracle.com/otn-pub/java/jce/${JAVA_VERSION_MAJOR}/jce_policy-${JAVA_VERSION_MAJOR}.zip" "${DOWNLOAD_DIR}"
 fi
-tar -xzf $ORIGINAL_PACKAGE
-unzip -q $JCE_PACKAGE
+tar -xzf $ORIGINAL_PACKAGE -C ${TEMP_DIR}
+unzip -q $JCE_PACKAGE -d ${TEMP_DIR}
 
 # add the updated JCE policy files
 mv ${JCE_DIRECTORY}/*.jar "${JDK_DIRECTORY}/jre/lib/security/"
 # set the egd to /dev/urandom
 sed -i.bak 's;securerandom.source=.*;securerandom.source=file:/dev/urandom;g' ${JDK_DIRECTORY}/jre/lib/security/java.security
-mkdir -p target
 
 if [ -f $FINAL_ARTIFACT ]; then
   rm $FINAL_ARTIFACT
@@ -109,6 +118,5 @@ fi
 tar -czf $FINAL_ARTIFACT $JDK_DIRECTORY
 
 # clean up
-rm -rf $JCE_DIRECTORY
-rm -rf $JDK_DIRECTORY
+rm -rf $TEMP_DIR
 
